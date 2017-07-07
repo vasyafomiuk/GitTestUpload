e3bd2c821371072d6b802fc26d50b938255897b1
vasyafomiuk <vasya23fomiuk@gmail.com>
2017-07-07 15:51:23 -0400

    Update

--- src/test/java/com/cbt/AppTest.java
+++ src/test/java/com/cbt/AppTest.java
@@ -3,7 +3,7 @@ package com.cbt;
 public class AppTest {
 
 	public static void main(String[] args) {
-		System.out.println("Test GIT project");
+		System.out.println("Test GIT project test");
 
 	}
 
